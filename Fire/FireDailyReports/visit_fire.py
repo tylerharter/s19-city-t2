1ac4a5bc5d6a1ccabd1906907497c10d07a8265b
Anna AD <anna.arpacidusseau@gmail.com>
2019-03-14 07:16:35 -0500

    Added time data and better y-Axis for plots

--- Fire/FireDailyReports/visit_fire.py
+++ Fire/FireDailyReports/visit_fire.py
@@ -3,15 +3,12 @@ from bs4 import BeautifulSoup
 import re
 import sys
 
-
-
-
 file = open(sys.argv[1],"w+");
 
-fulltext = "Incident,Date,Incident Type,Address,Updated\n"
+fulltext = "Incident,Date,Incident Type,Address,Time\n"
 
 
-for i in range(100,101):
+for i in range(0,145):
 
     x = urllib.request.urlopen("https://www.cityofmadison.com/fire/daily-reports?page="+str(i))
     html_doc = x.read().decode('utf-8')
@@ -21,9 +18,15 @@ for i in range(100,101):
     for br in soup.find_all("br"):
         br.replace_with(", ")
 
+    times = []
+
     text = ""
-    for link in soup.find_all(class_ = "view-content")[2].find_all(class_="row"):
-        text += link.get_text();
+    for link in soup.find_all(class_ = "views-display")[2].find_all(class_="row"):
+        if(":" in link.get_text()):
+            text += link.get_text().strip()
+        if(link.find(property="dc:date") != None):
+            times.append(link.find(property="dc:date")["content"])
+        text += "\n"
 
    # lines = (line.strip() for line in text.splitlines())
    # chunks = (phrase.strip() for line in lines for phrase in line.split(" "))
@@ -31,21 +34,17 @@ for i in range(100,101):
     text = text.strip()
    
     newtext = ""
-    itemcount = 1
+    itemcount = 0
     for a in text.split("\n"):
-        for arg in a.split(":  "):
-            arg = arg.strip()
-            if arg != "Incident" and arg != "Date" and arg != "Updated" and arg != "Incident Type" and arg != "Address":
-                arg  = arg.replace("\"","'").replace("“","'")
-                if itemcount % 5 != 0:   
-                    newtext += '"' + arg + '",'
-                else:
-                    newtext += '"' + arg + '"\n'
-                itemcount += 1;
-    newtext += '""\n'
+        args = a.split(": ");
+        args[1] = args[1].replace("\"","'").replace("“","'").strip()
+        if args[0].strip() != "Updated":
+            newtext += '"' + args[1] + '",'
+        if args[0].strip() == "Address":
+            newtext += '"' + times[int(itemcount)] + '"\n'
+            itemcount+=1
     fulltext += newtext
     print ("Page " + str(i) + " downloaded...")
 
 print("Download complete!")
-print(fulltext)
 file.write(fulltext)
